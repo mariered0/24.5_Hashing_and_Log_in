87f11513f764d532d9af93ff4b9e89ff953e7582
mariered0 <98713565+mariered0@users.noreply.github.com>
2022-08-15 15:15:58 -0700

    add heroku psql url

--- app.py
+++ app.py
@@ -7,14 +7,10 @@ from forms import AddFeedbackForm, UserForm, LoginForm
 import os
 
 app = Flask(__name__)
-app.config["SQLALCHEMY_DATABASE_URI"] = "postgresql:///feedback_db"
+app.config["SQLALCHEMY_DATABASE_URI"] = os.environ.get('DATABASE_URL', 'postgresql:///feedback_db')
 app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 app.config["SQLALCHEMY_ECHO"] = True
 app.config["SECRET_KEY"] = os.environ.get('SECRET_KEY', 'hellosecret1')
-print('*********************************')
-print('*********************************')
-print('*********************************')
-print(app.config['SECRET_KEY'])
 app.config['DEBUG_TB_INTERCEPT_REDIRECTS'] = False
 
 
